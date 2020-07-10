fe67c05865592107e92af0ed36575d57ed16dc46
jsnap96 <61860100+jsnap96@users.noreply.github.com>
2020-07-10 15:12:02 -0400

    Update Area.java

--- Area.java
+++ Area.java
@@ -4,7 +4,7 @@ public class Area {
 
 	public static void main(String[] args) {
 		/* This program calculates the area of a pentagon and asks the user to enter the length from the center of a pentagon to a vertex and 
-		computes the area of the pentagon Chapter 4 problem */
+		computes the area of the pentagon Chapter 4 problem 4.1 */
 		
 		
 		
@@ -19,7 +19,7 @@ public class Area {
 		
 		double length = 2*side*Math.sin(Math.PI/5);
 
-		double Area = (4*length*length)/(4*Math.tan(Math.PI/5));
+		double Area = (5*length*length)/(4*Math.tan(Math.PI/5));
 		
 		
 		System.out.print("The area of the Pentagon is " + String.format("%.2f", Area));
